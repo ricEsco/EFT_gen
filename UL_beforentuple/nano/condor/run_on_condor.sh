c5ef4466f494fa8c94526d696313459d045d615b
Beren Ozek <bozek3@uic.edu>
2024-02-28 01:36:20 +0100

    last edits for the nanogen file scripts

---
+++ UL_beforentuple/nano/condor/run_on_condor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /cvmfs/grid.desy.de/etc/profile.d/grid-ui-env.sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+export SCRAM_ARCH=slc7_amd64_gcc820
+scramv1 project CMSSW CMSSW_10_6_28 # cmsrel is an alias not on the workers
+cd CMSSW_10_6_28/src/
+eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers
+echo $CMSSW_BASE "is the CMSSW we created on the local worker node"
+cd ${_CONDOR_SCRATCH_DIR}
+pwd
+
+root_file_path="/nfs/dust/cms/user/beozek/EFT/CMSSW_10_6_27/src/EFT_gen_old/UL_beforentuple/nano/120000/0520A050-AF68-EF43-AA5B-5AA77C74ED73.root"
+
+xrdcp -f /nfs/dust/cms/user/beozek/EFT/CMSSW_10_6_27/src/EFT_gen_old/UL_beforentuple/nano/UL_nano_fully_semileptonic_multiprocess.py .
+python UL_nano_fully_semileptonic_multiprocess.py "$root_file_path"
